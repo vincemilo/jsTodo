741bfb0574f32f5ea4953ee0a4c93fb24cb952c7
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2023-12-01 15:12:45 -0700

    start work on projects button

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { projects } from "./projects";
 import './style.css';
 
 const content = document.getElementById('content');
+const main = document.createElement('main');
 
 content.appendChild(header());
-content.appendChild(newTask());
-buttonListeners();
+content.appendChild(main);
+//main.appendChild(newTask());
+//buttonListeners();
 
 const navButtons = document.querySelector('.nav-buttons');
 
@@ -16,16 +18,34 @@ navButtons.addEventListener('click', (e) => {
     // let main = document.querySelector('main');
     let button = e.target.className;
     if (button === 'projects btn'){
-        console.log('projects');
-        //content.replaceChild(home(), main);
+        main.innerText = '';
+        const projectsDiv = document.createElement('div');
+        const projectList = projects.getProjects();
+        for (let project of projectList){
+            if (project.name === "Add To New Project"){
+                continue;
+            } else {
+                const proj = document.createElement('div');
+                proj.innerText = project.name;
+                proj.addEventListener('click', () => {
+                    const ul = document.createElement('ul');
+                    const li = document.createElement('li');
+                    console.log(project.info);
+                });
+                projectsDiv.appendChild(proj);    
+            };
+        }
+        
+        main.appendChild(projectsDiv);
     } else if (button === 'new btn'){
         const taskForm = document.querySelector('.new-task');
         if (taskForm === null) {
-            content.appendChild(newTask());
+            main.innerText = '';
+            main.appendChild(newTask());
             buttonListeners();
         } else {
-            taskForm.remove();
-            content.appendChild(newTask());
+            main.innerText = '';
+            main.appendChild(newTask());
             buttonListeners();
         };
     };
