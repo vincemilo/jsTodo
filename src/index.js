93e9410cfcb391c7f289d002eab387e9fd091399
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2023-12-21 10:15:19 -0700

    added dropdown description logic on task click

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ const main = document.createElement('main');
 content.appendChild(header());
 content.appendChild(main);
 
-const task = new Task('blah', '10/10/2020', 'Uncategorized', 'descrip');
-const task2 = new Task('blahdsf', '10/12/2020', 'Uncategorized', 'descrup2');
+const task = new Task('blah', '10/10/2020', 'Uncategorized', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+const task2 = new Task('blahdsf', '10/12/2020', 'New Project', 'descrup2');
 
 addTaskToProject('Uncategorized', task);
-addTaskToProject('Uncategorized', task2);
+addTaskToProject('New Project', task2);
 displayProjects();
 
 const navButtons = document.querySelector('.nav-buttons');
--- src/taskDetails.js
+++ src/taskDetails.js
@@ -2,12 +2,29 @@ export default function taskDetails(task, taskDiv){
     console.log(task);
     //console.log(taskDiv.className);
     const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('task-descrip');
     descriptionDiv.innerText = task['description'];
     if (taskDiv.className === 'task'){
+        const projDiv = document.getElementById(task['project']);
+        console.log(projDiv);
+        const btns = document.createElement('div');
+        const editBtn = document.createElement('button');
+        const delBtn = document.createElement('button');
+        editBtn.innerText = 'Edit';
+        delBtn.innerText = 'Delete';
+        btns.classList.add('buttons');
+        btns.addEventListener('click', (e)=> {
+            //e.stopPropagation();
+            console.log('click')
+        });
+        btns.appendChild(editBtn);
+        btns.appendChild(delBtn);
+        descriptionDiv.appendChild(btns);
+        projDiv.appendChild(descriptionDiv);
         taskDiv.classList.toggle('toggle');
-        taskDiv.appendChild(descriptionDiv);
+
     } else if (taskDiv.className === 'task toggle'){
-        taskDiv.lastChild.remove();
+        taskDiv.nextSibling.remove();
         taskDiv.classList.toggle('toggle');
     };
     
