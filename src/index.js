6da4ca523251e8cead5cda40a8be8176d88cf920
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2024-01-25 12:49:34 -0700

    dynamic sizing for mobile devices/portrait mode

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ import footer from "./footer";
 
 const content = document.getElementById('content');
 const main = document.createElement('main');
+const wrapper = document.createElement('div');
+wrapper.id = 'wrapper';
 
-content.appendChild(header());
-content.appendChild(main);
+wrapper.appendChild(header());
+wrapper.appendChild(main);
+content.appendChild(wrapper);
 content.appendChild(footer());
 
 const task = new Task('Lorem ipsum', '2020-10-10', 'Uncategorized', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
